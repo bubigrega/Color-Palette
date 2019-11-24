b3068d0f1b37734b85b78aa8c101e10d6fc1773e
Gregor Ahcin <bubi.grega@gmail.com>
2019-11-24 23:33:25 +0100

    added colorbox component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import ColorPalettes from "./ColorPallete";
+import Palette from "./Palette";
 
 function App() {
-  return <div className="App">Pallete</div>;
+  return (
+    <div className="App">
+      <Palette {...ColorPalettes[0]} />
+    </div>
+  );
 }
 
 export default App;
--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
 class Palette extends Component {
   render() {
+    let colors = this.props.colors.map(c => (
+      <ColorBox key={c.id} background={c.color} name={c.name} />
+    ));
     return (
-      <div>
-        <div>Palette</div>
+      <div className="Palette">
+        <div className="Palette-colors">{colors}</div>
       </div>
     );
   }
